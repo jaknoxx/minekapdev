2777afa88f4f6ea745c4c6740907fd19a65a4308
jaknox <jakubew.eu@gmail.com>
2026-04-11 19:19:36 +0200

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -74,6 +74,7 @@ function debounce(func, delay) {
 
 // Escape HTML
 function escapeHtml(text) {
+    if (!text) return '';
     const div = document.createElement('div');
     div.textContent = text;
     return div.innerHTML;
